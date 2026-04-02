1ce95508d90cf5e7636e35ecb936cb0be2a1001a
Star-Bloom <beahmendessss@gmail.com>
2026-04-02 20:26:10 -0300

    Add files via upload

---
+++ Ex11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	double valor1;
+	double valor2;
+	double resultado;
+	
+	cout << "Insira o primeiro valor: ";
+	cin >> valor1;
+	cout << "Insira o segundo valor: ";
+	cin >> valor2;
+	
+	resultado = (valor1, valor2);
+	
+	cout << "O valor calculado: " << resultado;
+	return 0;
+}
